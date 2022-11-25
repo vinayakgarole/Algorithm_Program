597342dbcb4bfdd8c24a3f0666da9626d19a5771
vinayakgarole <vinayakgarole10@gmail.com>
2022-11-25 23:41:57 +0530

    Added: Insertion the sort

--- src/com/bridgelabz/BubbleSort.java
+++ src/com/bridgelabz/BubbleSort.java
@@ -9,10 +9,9 @@ public class BubbleSort<T extends Comparable<T>> {
 
         for (int i = 0; i < a.length; i++) {
 
-            for (int j = 0; j < a.length - 1 - i; j++) {
+            for (int j = 0; j < a.length - 1; j++) {
 
                 if (a[j] > a[j + 1]) {
-
                     temp = a[j];
                     a[j] = a[j + 1];
                     a[j + 1] = temp;
