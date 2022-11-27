4469a171835de8b632ec75f619a5bd1f1f7229ec
vinayakgarole <vinayakgarole10@gmail.com>
2022-11-27 18:39:38 +0530

    Added: If the search word is found in the list

--- src/com/bridgelabz/BinarySearchWord.java
+++ src/com/bridgelabz/BinarySearchWord.java
@@ -12,17 +12,20 @@ public class BinarySearchWord {
         String x = "Mumbai";
         int result = binarySearch(arr, x);
 
-        if (result == -1)
+        if (result == -1) {
             System.out.println("Element not present");
-        else
+        } else {
             System.out.println("Element found at " + "index " + result);
+        }
     }
 
     static int binarySearch(String[] arr, String x) {
         int l = 0, r = arr.length - 1;
+
         while (l <= r) {
             int m = l + (r - l) / 2;
             int res = x.compareTo(arr[m]);
+
             if (res == 0)
                 return m;
             if (res > 0)
